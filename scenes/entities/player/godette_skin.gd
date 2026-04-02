8b5a2a15990dd3b79dc5690d015780f9f01d6937
lazy <84917403+Lazy13909@users.noreply.github.com>
2026-04-02 21:46:01 +0500

    Added three animations

--- scenes/entities/player/godette_skin.gd
+++ scenes/entities/player/godette_skin.gd
@@ -1,6 +1,44 @@
 extends Node3D
 
-@onready var move_state_machine = $AnimationTree.get("parameters/playback")
+@onready var move_state_machine = $AnimationTree.get("parameters/MovementStateMachine/playback")
+@onready var attack_state_machine = $AnimationTree.get("parameters/AttackStateMachine/playback")
+@onready var extra_animation = $AnimationTree.get_tree_root().get_node('ExtraAnimation')
+
+var attacking := false
 
 func set_move_state(state_name: String) -> void:
 	move_state_machine.travel(state_name)
+
+func attack() -> void:
+	if not attacking:
+		attack_state_machine.travel('Slice' if $SecondAttackTimer.time_left else 'Chop')
+		$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+func attack_toggle(value: bool):
+	attacking = value
+
+func defend(forward: bool) -> void: 
+	var tween = create_tween()
+	tween.tween_method(_defend_change, 1.0 - float(forward), float(forward), 0.25)
+	
+func _defend_change(value: float) -> void:
+	$AnimationTree.set("parameters/ShieldBlend/blend_amount", value)
+	
+func switch_weapon(weapon_active: bool) -> void:
+	if weapon_active:
+		$Rig/Skeleton3D/RightHandSlot/sword_1handed2.show()
+		$Rig/Skeleton3D/RightHandSlot/wand2.hide()
+	else:
+		$Rig/Skeleton3D/RightHandSlot/sword_1handed2.hide()
+		$Rig/Skeleton3D/RightHandSlot/wand2.show()
+		
+func cast_spell() -> void:
+	if not attacking:
+		extra_animation.animation = 'Spellcast_Shoot'
+		$AnimationTree.set("parameters/ExtraOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+func hit() -> void:
+	extra_animation.animation = 'Hit_A'
+	$AnimationTree.set("parameters/ExtraOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	$AnimationTree.set("parameters/AttackOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
+	attacking = false
--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -8,16 +8,31 @@ extends CharacterBody3D
 @onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
 @onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
+@onready var godette_skin = $GodetteSkin
 @onready var camera_controller = $CameraController/Camera3D
 
 @export var base_speed := 4.0
 @export var run_speed := 8.0
+@export var defend_speed := 2.0
 
 var movement_input = Vector2.ZERO
+var speed_modifier := 1.0
+
+var defend := false:
+	set(value):
+		if not defend and value:
+			godette_skin.defend(true)
+		if defend and not value:
+			godette_skin.defend(false)
+		defend = value
+var weapon_active := false
 
 func _physics_process(delta: float) -> void:
 	move_logic(delta)
 	jump_logic(delta)
+	ability_logic()
+	if Input.is_action_just_pressed("ui_accept"):
+		godette_skin.hit()
 	move_and_slide()
 
 func move_logic(delta) -> void:
@@ -27,22 +42,50 @@ func move_logic(delta) -> void:
 	
 	if movement_input != Vector2.ZERO:
 		var speed = run_speed if is_running else base_speed
+		speed = defend_speed if defend else speed
 		
 		vel_2d += movement_input * speed * delta
-		vel_2d = vel_2d.limit_length(speed)
+		vel_2d = vel_2d.limit_length(speed) * speed_modifier
 		velocity.x = vel_2d.x
 		velocity.z = vel_2d.y
-		$GodetteSkin.set_move_state('Running')
+		godette_skin.set_move_state('Running')
 		var target_angle = -movement_input.angle() + PI/2
-		$GodetteSkin.rotation.y = rotate_toward($GodetteSkin.rotation.y, target_angle, 6.0 * delta)
+		godette_skin.rotation.y = rotate_toward(godette_skin.rotation.y, target_angle, 6.0 * delta)
 	else:
 		vel_2d = vel_2d.move_toward(Vector2.ZERO, base_speed * 4.0 * delta)
 		velocity.x = vel_2d.x
 		velocity.z = vel_2d.y
-		$GodetteSkin.set_move_state('Idle')
+		godette_skin.set_move_state('Idle')
 		
 func jump_logic(delta) -> void:
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_velocity
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -jump_velocity
+	else:
+		godette_skin.set_move_state('Jump')
 	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
 	velocity.y -= gravity * delta
+
+func ability_logic() -> void: 
+	if Input.is_action_just_pressed("ability"):
+		if weapon_active:
+			godette_skin.attack()
+			stop_movement(0.3, 0.5)
+		else:
+			godette_skin.cast_spell()
+			stop_movement(0.3, 0.8)
+	
+	defend = Input.is_action_pressed("block")
+	
+	if Input.is_action_just_pressed("switch_weapon") and not godette_skin.attacking:
+		weapon_active = not weapon_active
+		godette_skin.switch_weapon(weapon_active)
+
+func stop_movement(start_duration: float, end_duration: float):
+	var tween = create_tween()
+	tween.tween_property(self, "speed_modifier", 0.0, start_duration)
+	tween.tween_property(self, "speed_modifier", 1.0, end_duration)
+	
+func hit():
+	godette_skin.hit()
+	stop_movement(0.3, 0.5)
